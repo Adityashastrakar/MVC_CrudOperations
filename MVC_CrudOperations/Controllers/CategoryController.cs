c5c90c25335564acdd7bf5fd619639d568811abc
ADITYA SHASTRAKAR <adityashastrakar123@gmail.com>
2023-07-03 10:12:53 +0530

    2ndCommit

---
+++ MVC_CrudOperations/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_CrudOperations.Data;
+
+namespace MVC_CrudOperations.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public CategoryController(ApplicationContext context)
+        {
+            this.context=context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await context.Categories.ToListAsync();
+            return View(data);
+        }
+    }
+}
